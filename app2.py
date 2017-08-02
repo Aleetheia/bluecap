b81fbad22203c2cd2a7a85439a98659dd2668274
Dounia <aleetheia@gmal.com>
2017-08-02 14:11:45 +0200

    no message

--- app2.py
+++ app2.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, json, flash, session
+from flask import Flask, render_template, request, json, flash, session, redirect
 #from flask.ext.mysql import MySQL
 import MySQLdb as sql
 
